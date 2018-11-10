e11461e5d5c261b5f96ab1eabbafb3c5a715375d
sraju728@gmail.com <sraju728@gmail.com>
2018-11-10 16:45:20 +0530

    Creating the model2 with data augumentation and plotted the results

--- ConvolutionalNeuralNetworkAndAutoencoderParallel.py
+++ ConvolutionalNeuralNetworkAndAutoencoderParallel.py
@@ -175,3 +175,53 @@ plt.legend(['Training loss', 'Validation Loss'],fontsize=18)
 plt.xlabel('Epochs ',fontsize=16)
 plt.ylabel('Loss',fontsize=16)
 plt.title('Loss Curves',fontsize=16)
+
+model2 = createModel()
+
+model2.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+batch_size = 256
+epochs = 150
+datagen = ImageDataGenerator(
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        horizontal_flip=True,
+        vertical_flip=False)
+
+history2 = model2.fit_generator(datagen.flow(train_data, train_labels_one_hot, batch_size=batch_size),
+                              steps_per_epoch=int(np.ceil(train_data.shape[0] / float(batch_size))),
+                              epochs=epochs,
+                              validation_data=(test_data, test_labels_one_hot),
+                              workers=4)
+
+model2.evaluate(test_data, test_labels_one_hot)
+
+
+
+plt.figure(figsize=[8,6])
+plt.plot(history2.history['loss'],'r',linewidth=3.0)
+plt.plot(history2.history['val_loss'],'b',linewidth=3.0)
+plt.legend(['Training loss', 'Validation Loss'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Loss',fontsize=16)
+plt.title('Loss Curves',fontsize=16)
+
+
+
+plt.figure(figsize=[8,6])
+plt.plot(history.history['acc'],'r',linewidth=3.0)
+plt.plot(history.history['val_acc'],'b',linewidth=3.0)
+plt.legend(['Training Accuracy', 'Validation Accuracy'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Accuracy',fontsize=16)
+plt.title('Accuracy Curves',fontsize=16)
+
+
+
+plt.figure(figsize=[8,6])
+plt.plot(history2.history['acc'],'r',linewidth=3.0)
+plt.plot(history2.history['val_acc'],'b',linewidth=3.0)
+plt.legend(['Training Accuracy', 'Validation Accuracy'],fontsize=18)
+plt.xlabel('Epochs ',fontsize=16)
+plt.ylabel('Accuracy',fontsize=16)
+plt.title('Accuracy Curves',fontsize=16)
